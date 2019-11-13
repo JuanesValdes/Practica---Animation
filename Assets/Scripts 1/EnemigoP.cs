929ec6b76513310995c7f62951e34729e7bf1966
Juan Valdes <mw3_america@hotmail.com>
2019-11-12 18:38:39 -0600

    edits

---
+++ Assets/Scripts 1/EnemigoP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoP : Persecucion
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Perseguir(playerPosition, this.gameObject.transform);
+    }
+}
---
+++ Assets/Scripts 1/Persecucion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persecucion : MonoBehaviour
+{
+    public Transform playerPosition;
+    public float velocidadEnemigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Perseguir(playerPosition,this.gameObject.transform);
+    }
+
+    public void Perseguir (Transform player, Transform enemigo)
+    {
+        Vector3 distancia = player.position - enemigo.position;
+
+        enemigo.LookAt(distancia);
+        enemigo.Translate(distancia.normalized * velocidadEnemigo * Time.deltaTime);
+    }
+}
